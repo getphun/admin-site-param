fba2bd90b6705d2f75f6b1ec3c2d004e3cc1320a
iqbalfn <iqbalfawz@gmail.com>
2017-07-24 15:36:37 +0700

    Update sql to insert only if not exists

--- modules/admin-site-param/_db/install.sql
+++ modules/admin-site-param/_db/install.sql
@@ -1,4 +1,4 @@
-INSERT INTO `user_perms` ( `name`, `group`, `role`, `about` ) VALUES
+INSERT IGNORE INTO `user_perms` ( `name`, `group`, `role`, `about` ) VALUES
     ( 'create_setting', 'Site Params', 'Management', 'Allow user to create new site setting' ),
     ( 'read_setting',   'Site Params', 'Management', 'Allow user to view all site settings' ),
     ( 'remove_setting', 'Site Params', 'Management', 'Allow user to remove exists site setting' ),
